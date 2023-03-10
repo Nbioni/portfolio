ef5fa38dab9804bf0c5ec5179732bee6c2ed7025
Nbioni <43020119+Nbioni@users.noreply.github.com>
2023-03-10 15:33:56 -0300

    Add page title

--- lib/app/modules/home/presenter/views/home_view.dart
+++ lib/app/modules/home/presenter/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:portfolio/app/core/utils/definitions/colors.dart';
 
 import '../../../../core/presenter/widgets/animated_background/animated_background.dart';
 import '../../../../core/presenter/widgets/app_scaffold/app_scaffold.dart';
@@ -24,55 +25,60 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return AppScaffold(
-      backgroundColor: _homeController.backgroundColor,
-      coreController: _homeController.core,
-      child: AppAnimatedBackground(
-        child: Stack(
-          children: [
-            Container(
-              alignment: Alignment.centerRight,
-              child: ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: size.height * 0.9),
-                child: AspectRatio(
-                  aspectRatio: 640 / 868,
-                  child: LayoutBuilder(builder: (context, constraints) {
-                    return const AvatarModelViewer();
-                  }),
+    return Title(
+      title: "Nahaliel Bioni",
+      color: Colors.white,
+      child: AppScaffold(
+        backgroundColor: _homeController.backgroundColor,
+        coreController: _homeController.core,
+        child: AppAnimatedBackground(
+          child: Stack(
+            children: [
+              Container(
+                alignment: Alignment.centerRight,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxHeight: size.height * 0.9),
+                  child: AspectRatio(
+                    aspectRatio: 640 / 868,
+                    child: LayoutBuilder(builder: (context, constraints) {
+                      return const AvatarModelViewer();
+                    }),
+                  ),
                 ),
               ),
-            ),
-            GestureDetector(
-              onVerticalDragUpdate: (details) {
-                _homeController.onScrollEvent(-details.delta.dy);
-              },
-              child: Listener(
-                onPointerSignal: (pointerSignal) {
-                  if (pointerSignal is PointerScrollEvent) {
-                    _homeController.onScrollEvent(pointerSignal.scrollDelta.dy);
-                  }
+              GestureDetector(
+                onVerticalDragUpdate: (details) {
+                  _homeController.onScrollEvent(-details.delta.dy);
                 },
-                child: PageView(
-                  controller: _homeController.pageController,
-                  scrollDirection: Axis.vertical,
-                  padEnds: false,
-                  physics: const NeverScrollableScrollPhysics(),
-                  scrollBehavior: AppScrollBehavior(),
-                  children: [
-                    Session0(
-                      backgroundColor: _homeController.backgroundColor,
-                      onJumpClick: () {
-                        _homeController.setCurrentSession(1);
-                        _homeController.jumpToSession1();
-                      },
-                    ),
-                    const Session1(),
-                    Session2(homeController: _homeController),
-                  ],
+                child: Listener(
+                  onPointerSignal: (pointerSignal) {
+                    if (pointerSignal is PointerScrollEvent) {
+                      _homeController
+                          .onScrollEvent(pointerSignal.scrollDelta.dy);
+                    }
+                  },
+                  child: PageView(
+                    controller: _homeController.pageController,
+                    scrollDirection: Axis.vertical,
+                    padEnds: false,
+                    physics: const NeverScrollableScrollPhysics(),
+                    scrollBehavior: AppScrollBehavior(),
+                    children: [
+                      Session0(
+                        backgroundColor: _homeController.backgroundColor,
+                        onJumpClick: () {
+                          _homeController.setCurrentSession(1);
+                          _homeController.jumpToSession1();
+                        },
+                      ),
+                      const Session1(),
+                      Session2(homeController: _homeController),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
